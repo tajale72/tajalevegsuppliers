b5ea684ae5b7b90586f24cc1bf16959e89c928bd
Tajale72 <tajale72@gmail.com>
2023-11-05 18:50:58 -0600

    organized code

--- myoneapp/db/db.go
+++ myoneapp/db/db.go
@@ -20,58 +20,65 @@ const (
 	sslmode  = "disable"
 )
 
-func GetDBConnection() (*sql.DB, error) {
+type DBClient struct {
+	DB *sql.DB
+}
+
+func GetDBConnection() (*DBClient, error) {
 	connStr := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?sslmode=%s", user, password, host, port, dbname, sslmode)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		log.Println("error conneting to postgres db", err)
-		return nil, err
+		return nil, fmt.Errorf("error connecting to postgres db: %w", err)
 	}
 
-	err = db.Ping()
-	if err != nil {
-		log.Println("error from pinging", err)
-		return nil, err
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("error pinging database: %w", err)
 	}
 
-	return db, nil
+	return &DBClient{DB: db}, nil
 }
 
-func CreateTable(db *sql.DB, data []byte) error {
+func (dbClient *DBClient) Close() {
+	if dbClient.DB != nil {
+		dbClient.DB.Close()
+	}
+}
+
+func (dbClient *DBClient) CreateTable(data []byte) error {
 	var req model.Request
-	err := json.Unmarshal(data, &req)
-	if err != nil {
+	if err := json.Unmarshal(data, &req); err != nil {
 		log.Println("Unmarshal", err)
-		return err
+		return fmt.Errorf("error unmarshaling request: %w", err)
 	}
 
 	// Convert the products slice to a JSON string
 	productsJSON, err := json.Marshal(req.Products)
 	if err != nil {
 		log.Println("Marshal", err)
-		return err
+		return fmt.Errorf("error marshaling products: %w", err)
 	}
 
 	// SQL statement with placeholders
 	sqlStatement := `
     INSERT INTO Bill_Details (bill_name, bill_date, bill_place, bill_number, customer_phonenumber, customer_pan_num, products, bill_total_amount)
-VALUES ($1, $2, $3, $4::jsonb, $5, $6, $7, $8);`
+    VALUES ($1, $2, $3, $4::jsonb, $5, $6, $7, $8);`
 
 	// Execute the SQL query with specific values
-	_, err = db.Exec(sqlStatement, req.BillName, req.BillDate, req.BillPlace, req.BillNumber, req.CustomerPhonenumber, req.CustomerPanNum, productsJSON, req.BillTotalAmount)
+	_, err = dbClient.DB.Exec(sqlStatement, req.BillName, req.BillDate, req.BillPlace, req.BillNumber, req.CustomerPhonenumber, req.CustomerPanNum, productsJSON, req.BillTotalAmount)
 	if err != nil {
 		log.Println("error Exec statement", err)
-		return err
+		return fmt.Errorf("error executing SQL statement: %w", err)
 	}
 
 	return nil
 }
 
-func GetProducts(db *sql.DB) ([]model.Request, error) {
-	rows, err := db.Query("SELECT * FROM Bill_Details;")
+func (dbClient *DBClient) GetProducts() ([]model.Request, error) {
+	rows, err := dbClient.DB.Query("SELECT * FROM Bill_Details;")
 	if err != nil {
 		log.Println("Error querying database:", err)
-		return nil, err
+		return nil, fmt.Errorf("error querying database: %w", err)
 	}
 	defer rows.Close()
 
@@ -81,55 +88,61 @@ func GetProducts(db *sql.DB) ([]model.Request, error) {
 		var productsJSON string
 		if err := rows.Scan(&bill.ID, &bill.BillName, &bill.BillDate, &bill.BillPlace, &productsJSON, &bill.BillNumber, &bill.CustomerPanNum, &bill.CustomerPhonenumber, &bill.BillTotalAmount); err != nil {
 			log.Println("Error scanning row:", err)
-			return nil, err
+			return nil, fmt.Errorf("error scanning row: %w", err)
 		}
 
 		// Unmarshal the JSON string into the Products field of the bill
 		if err := json.Unmarshal([]byte(productsJSON), &bill.Products); err != nil {
 			log.Println("Error unmarshaling products:", err)
-			return nil, err
+			return nil, fmt.Errorf("error unmarshaling products: %w", err)
 		}
 		listOfBills = append(listOfBills, bill)
 	}
 
 	if err := rows.Err(); err != nil {
 		log.Println("Error iterating over rows:", err)
-		return nil, err
+		return nil, fmt.Errorf("error iterating over rows: %w", err)
 	}
 
 	return listOfBills, nil
 }
 
-func GetProductByID(db *sql.DB, id int) (model.Request, error) {
+func (dbClient *DBClient) GetProductByID(id int) (model.Request, error) {
 	var bill model.Request
 	var productsJSON string
 
-	err := db.QueryRow("SELECT * FROM Bill_Details WHERE id = $1;", id).
+	err := dbClient.DB.QueryRow("SELECT * FROM Bill_Details WHERE id = $1;", id).
 		Scan(&bill.ID, &bill.BillName, &bill.BillDate, &bill.BillPlace, &productsJSON, &bill.BillNumber, &bill.CustomerPanNum, &bill.CustomerPhonenumber, &bill.BillTotalAmount)
 	if err != nil {
 		log.Println("Error querying database:", err)
-		return bill, err
+		return bill, fmt.Errorf("error querying database: %w", err)
 	}
 
 	// Unmarshal the JSON string into the Products field of the bill
 	if err := json.Unmarshal([]byte(productsJSON), &bill.Products); err != nil {
 		log.Println("Error unmarshaling products:", err)
-		return bill, err
+		return bill, fmt.Errorf("error unmarshaling products: %w", err)
 	}
 
 	return bill, nil
 }
 
-func updateUser(db *sql.DB, id int, newUsername, newEmail string) error {
-	_, err := db.Exec("UPDATE users SET username=$1, email=$2 WHERE id=$3", newUsername, newEmail, id)
-	log.Println("")
-	return err
+func (dbClient *DBClient) UpdateUser(id int, newUsername, newEmail string) error {
+	_, err := dbClient.DB.Exec("UPDATE users SET username=$1, email=$2 WHERE id=$3", newUsername, newEmail, id)
+	if err != nil {
+		log.Println("Error updating user:", err)
+		return fmt.Errorf("error updating user: %w", err)
+	}
+	return nil
 }
 
-func deleteUser(db *sql.DB, id int) error {
-	_, err := db.Exec("DELETE FROM users WHERE id=$1", id)
-	log.Println("")
-	return err
+func (dbClient *DBClient) DeleteUser(id int) error {
+	_, err := dbClient.DB.Exec("DELETE FROM users WHERE id=$1", id)
+	if err != nil {
+		log.Println("Error deleting user:", err)
+		return fmt.Errorf("error deleting user: %w", err)
+	}
+	return nil
 }
 
 type User struct {
@@ -137,37 +150,3 @@ type User struct {
 	Username string
 	Email    string
 }
-
-// func main() {
-// 	db, err := getDBConnection()
-// 	if err != nil {
-// 		log.Fatal("Error connecting to the database: ", err)
-// 	}
-// 	defer db.Close()
-
-// 	// Example usage
-// 	err = createUser(db, "john_doe", "john@example.com")
-// 	if err != nil {
-// 		log.Fatal("Error creating user: ", err)
-// 	}
-
-// 	users, err := getUsers(db)
-// 	if err != nil {
-// 		log.Fatal("Error fetching users: ", err)
-// 	}
-
-// 	fmt.Println("Users:")
-// 	for _, user := range users {
-// 		fmt.Printf("ID: %d, Username: %s, Email: %s\n", user.ID, user.Username, user.Email)
-// 	}
-
-// 	err = updateUser(db, 1, "john_smith", "john.smith@example.com")
-// 	if err != nil {
-// 		log.Fatal("Error updating user: ", err)
-// 	}
-
-// 	err = deleteUser(db, 1)
-// 	if err != nil {
-// 		log.Fatal("Error deleting user: ", err)
-// 	}
-// }
